538d67b1b0772eb09b00dbaa1a0c190858f12390
gowsik-source <gowsik0426@gmail.com>
2026-06-07 10:04:01 +0530

    allowedpaths added

--- helper/allowedPaths.js
+++ helper/allowedPaths.js
@@ -1,7 +1,9 @@
 const allowedPaths = [
     '/api/user/login',
     '/api/user/register',
+    '/api/product/all-products',
     '/api/product/digital-cameras',
+    '/api/product/lenses-and-accessories',
 ];
 
 module.exports = allowedPaths;
